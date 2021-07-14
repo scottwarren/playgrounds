e4e7ba9d0ca6653f4a515d57a87545b838d2a0ef
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2021-07-14 14:15:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Calendar from 'react-calendar';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const DataDisplayContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DataDisplayContainer>
+        <h3>Selected Dates</h3>
+        <div>Start: {startDate.toISOString()}</div>
+        <div>End: {endDate.toISOString()}</div>
+      </DataDisplayContainer>
+      <Calendar onChange={([start, end]) => { setStartDate(start); setEndDate(end); }} maxDetail='year' minDetail="year" view="year" returnValue="range" />
+
     </div>
   );
 }
