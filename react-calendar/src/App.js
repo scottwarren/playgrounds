f96efc3ef811f9c2bc04836d1a7d54fd6c48795b
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2021-07-15 11:35:19 +0300

    fix: centering UI for nice visuals

--- react-calendar/src/App.js
+++ react-calendar/src/App.js
@@ -1,28 +1,36 @@
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import styled from 'styled-components';
 import { useState } from 'react';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
 const DataDisplayContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 function App() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
-
-
   return (
-    <div>
-      <DataDisplayContainer>
-        <h3>Selected Dates</h3>
-        <div>Start: {startDate.toDateString()}</div>
-        <div>End: {endDate.toDateString()}</div>
-      </DataDisplayContainer>
-      <Calendar onChange={([start, end]) => { setStartDate(start); setEndDate(end); }} maxDetail='year' minDetail="year" view="year" returnValue="range" />
-
-    </div>
+    <Container>
+      <div>
+        <DataDisplayContainer>
+          <h3>Selected Dates</h3>
+          <div>Start: {startDate.toDateString()}</div>
+          <div>End: {endDate.toDateString()}</div>
+        </DataDisplayContainer>
+        <Calendar onChange={([start, end]) => { setStartDate(start); setEndDate(end); }} maxDetail='year' view="year" returnValue="range" />
+      </div>
+    </Container>
   );
 }
 
