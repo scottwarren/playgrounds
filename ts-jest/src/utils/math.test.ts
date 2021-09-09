8b3a0769d8845113a9ef70307f895355ff52b10c
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2021-09-09 17:35:07 +0300

    chore: adds ts-jest playground

--- ts-jest/src/utils/math.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Math', () => {
-  it('add', async () => {
-    const result = await import('./math').then(({add}) => add(1, 2, 3));
-    expect(result).toEqual(6);
-    expect(result).toMatchSnapshot('snap 1');
-  });
-});
